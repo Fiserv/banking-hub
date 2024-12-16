a23ea64d54385571c2eaf909aa05a1eef6556fcd
AkashFiserv <akash.srivastava@fiserv.com>
2024-12-16 15:02:37 +0530

    Update replace-lines.js

--- replace-lines.js
+++ replace-lines.js
@@ -40,5 +40,5 @@ function processYamlFiles(directoryPath) {
 }
 
 // Replace 'your-directory-path' with the path to your directory containing .yaml files
-const directoryPath = './reference/11.0.0/Cards';
+const directoryPath = './reference/11.0.0/Transactions';
 processYamlFiles(directoryPath);
